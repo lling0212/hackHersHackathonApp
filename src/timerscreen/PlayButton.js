69219304fdc7c487262594b8a906f152694c4932
Nikita <niks19@seas.upenn.edu>
2024-01-26 22:41:17 -0500

    Adding toggle button and play button

--- src/timerscreen/PlayButton.js
+++ src/timerscreen/PlayButton.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import '/Users/nikitasingh/git/HackHers Project/hackHersHackathonApp/src/index.css'
 
-const PlayButton = () => {
+const PlayButton = ({onPlay}) => {
   return (
     <div>
+    <button
+    onClick={onPlay}
+    className='playbtn'
+    aria-label="Play"
+    >
+
+    <svg
+    viewBox="0 0 24 24"
+        width="24" height="24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="play-icon"
+    >
+      
+      <polygon points="5 3 19 12 5 21 5 3">
+
+      </polygon>
+      </svg>
+      </button>
+    
+    
       
     </div>
   )
